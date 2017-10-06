143ed7b92fe8a7fd3b80913d2c7cb50f36d36fa7
Boris Bakshiyev <bakshiyev@gmail.com>
2017-10-06 17:27:16 +0300

    misspelled protocol type

--- Ch03/UDPEchoServer.go
+++ Ch03/UDPEchoServer.go
@@ -12,7 +12,7 @@ import (
 func main() {
 
 	service := "0.0.0.0:1200"
-	udpAddr, err := net.ResolveUDPAddr("up4", service)
+	udpAddr, err := net.ResolveUDPAddr("udp4", service)
 	checkError(err)
 
 	conn, err := net.ListenUDP("udp", udpAddr)
